a23c90c7bd88133f1a9dd0c75e19b41d056e1ee0
Sean Lord <sean13lord@gmail.com>
2025-04-24 13:03:59 -0400

    Implemented concatenation-based profanity detection

--- profanity_detector.py
+++ profanity_detector.py
@@ -2,12 +2,22 @@ import string
 
 class Profanity:
 
-	def __init__(self, lyrics, phrase_indices, line_indices, profanity_level):
+	def __init__(self, caught_phrase, lyrics, start_index, end_index):
+		self.caught_phrase = caught_phrase # Raw lowercase phrase that was matched
 		self.lyrics = lyrics # Pointer to the original lyrics
-		self.phrase_indicies = phrase_indicies
-		self.line_indices = line_indices
-		self.profanity_level = profanity_level
+		self.start_index = start_index # Start index in original lyrics
+		self.end_index = end_index # End index in original lyrics
 
+	# Returns full line with profane phrase
+	def getLine(self):
+		prev_break = self.lyrics.rfind('\n', 0, self.start_index)
+		if prev_break == -1:
+			prev_break = 0
+		next_break = self.lyrics.find('\n', self.end_index)
+		if next_break == -1:
+			next_break = len(self.lyrics)
+		return self.lyrics[prev_break:next_break].strip()
+		
 
 class ConcatenationDetector:
 
@@ -15,13 +25,13 @@ class ConcatenationDetector:
 		self.trie = {}
 		self.remove_chars = f'{string.punctuation}{string.whitespace}'
 
-	def addProfanity(self, profanity, profanity_level):
+	def addProfanity(self, profanity):
 		curr_level = self.trie
 		for char in profanity:
 			if char not in curr_level:
 				curr_level[char] = {}
 			curr_level = curr_level[char]
-		curr_level['level'] = profanity_level	
+		curr_level['endnode'] = True
 
 	# Returns lyrics without whitespace or punctuation
 	def __concatLyrics(self, lyrics):
@@ -33,37 +43,62 @@ class ConcatenationDetector:
 				return_string += curr_char
 				array_map.append(i)
 		return return_string, array_map
-	
+
+	# Returns maximum length profanity starting at certain index
+	# Returns None if no profanity is found	
 	def __recursiveCheck(self, concat_lyrics, index, curr_trie):
 		if index >= len(concat_lyrics):
-			if 'level' in curr_trie: # Edge case
-				return '', curr_trie['level']
-			return None, None
+			if 'endnode' in curr_trie: # Edge case
+				return ''
+			return None
 		curr_char = concat_lyrics[index]
 		if curr_char not in curr_trie:
-			if 'level' in curr_trie:
-				return '', curr_trie['level']
-			return None, None
-		
-		
-			
-		
-	
+			if 'endnode' in curr_trie:
+				return ''
+			return None
+		recur_result = self.__recursiveCheck(concat_lyrics, index+1, curr_trie[curr_char])
+		if recur_result is not None:
+			return curr_char + recur_result
+		if 'endnode' in curr_trie:
+			return ''
+		return None 
+				
 	def checkLyrics(self, lyrics):
-		lyrics = lyrics.lower()
-		concat_lyrics = self.__concatLyrics(lyrics)
-		
+		lower_lyrics = lyrics.lower()
+		concat_lyrics, array_map = self.__concatLyrics(lower_lyrics)
 
+		profanities = [] # Contains Profanity objects
+		for i in range(0, len(concat_lyrics)):
+			checked = self.__recursiveCheck(concat_lyrics, i, self.trie)
+			if checked is not None:
+				concat_end_index = i + len(checked) - 1
+				true_start_index = array_map[i]
+				true_end_index = array_map[concat_end_index] + 1
+				profanity = Profanity(checked, lyrics, true_start_index, true_end_index)
+				profanities.append(profanity)
+		return profanities
+			
 
 class ProfanityClient:
 
-	def __init__(self, profanities, profanity_levels):
-		self.profanities = profanities
-		self.profanity_level = profanity_levels
+	def __init__(self, concat_profanities, isolate_profanities):
+		self.concat_profanities = concat_profanities
+		self.isolate_profanities = isolate_profanities
 
+		self.concat_checker = ConcatenationDetector()
+		for phrase in concat_profanities:
+			self.concat_checker.addProfanity(phrase)		
+
+	def checkLyrics(self, lyrics):
+		concat_results = self.concat_checker.checkLyrics(lyrics)
+		return concat_results
+		
 
 def main():
-	cd = ConcatenationDetector()
+	concat_profanities = ['igot', 'porsche', 'toma', 'hevall', 'chnow']
+	isolate_profanities = []
+
+	client = ProfanityClient(concat_profanities, isolate_profanities)
 	lyrics = """
 		I got the horses in the back
 		Horse tack is attached
@@ -74,7 +109,12 @@ def main():
 		I been in the valley
 		You ain't been up off that porch, now
 	"""
-	print(cd.concatLyrics(lyrics))
+	results = client.checkLyrics(lyrics)
+	for item in results:
+		caught = item.caught_phrase
+		line = item.getLine()
+		print(f'Caught "{caught}" in "{line}"')
+
 
 if __name__ == '__main__':
 	main()
